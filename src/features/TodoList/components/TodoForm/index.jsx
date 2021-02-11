8cee1396ed8c8ee43ab3706836d49e2a2d40a893
camha91 <crystalhanguyen@gmail.com>
2021-02-10 20:54:55 -0800

    Todo Form validation with Yup

--- src/features/TodoList/components/TodoForm/index.jsx
+++ src/features/TodoList/components/TodoForm/index.jsx
@@ -1,41 +1,63 @@
-import React, { useState } from 'react';
+import { yupResolver } from '@hookform/resolvers/yup';
 import PropTypes from 'prop-types';
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import InputField from '../../../../components/form-controls/InputField';
 
 TodoForm.propTypes = {
-    onSubmit: PropTypes.func,
+  onSubmit: PropTypes.func,
 };
 
-function TodoForm({ onSubmit }) {
-    const [value, setValue] = useState('');
+function TodoForm(props) {
+  //   const [value, setValue] = useState('');
 
-    function handleSubmit(e) {
-        e.preventDefault();
+  //   function handleSubmit(e) {
+  //     e.preventDefault();
 
-        const formValues = {
-            title: value,
-        }
+  //     const formValues = {
+  //       title: value,
+  //     };
 
-        if (!onSubmit || formValues.title === '') {
-            return;
-        }
-        onSubmit(formValues);
-        setValue('');
-    };
+  //     if (!onSubmit || formValues.title === '') {
+  //       return;
+  //     }
+  //     onSubmit(formValues);
+  //     setValue('');
+  //   }
 
-    function handleValueChange(e) {
-        const value = e.target.value;
-        setValue(value);
-    };
+  //   function handleValueChange(e) {
+  //     const value = e.target.value;
+  //     setValue(value);
+  //   }
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <input
-                type="text"
-                value={value}
-                onChange={handleValueChange}
-            />
-        </form>
-    );
+  const schema = yup.object().shape({
+    title: yup.string().required('Please enter title').min(5, 'Title is too short'),
+  });
+
+  const form = useForm({
+    defaultValues: {
+      title: '',
+    },
+    resolver: yupResolver(schema),
+  });
+
+  function handleSubmit(values) {
+    console.log('Todo form: ', values);
+  }
+
+  return (
+    // <form onSubmit={handleSubmit}>
+    //     <input
+    //         type="text"
+    //         value={value}
+    //         onChange={handleValueChange}
+    //     />
+    // </form>
+    <form onSubmit={form.handleSubmit(handleSubmit)}>
+      <InputField name="title" label="Todo" form={form} />
+    </form>
+  );
 }
 
 export default TodoForm;
--- src/features/TodoList/pages/ListPage/index.jsx
+++ src/features/TodoList/pages/ListPage/index.jsx
@@ -5,118 +5,120 @@ import TodoFeature from '../../components/TodoFeature';
 import TodoForm from '../../components/TodoForm';
 
 function ListPage() {
-    const initTodos = [
-        {
-            id: 1,
-            title: "Practice react hooks",
-            status: 'completed'
-        }, {
-            id: 2,
-            title: "Practice scss",
-            status: 'new'
-        }, {
-            id: 3,
-            title: "Edit resume",
-            status: 'new'
-        }, {
-            id: 4,
-            title: "Practice interview",
-            status: 'new'
-        }, {
-            id: 5,
-            title: "Apply for front end engineer jobs",
-            status: 'new'
-        }
-    ];
+  const initTodos = [
+    {
+      id: 1,
+      title: 'Practice react hooks',
+      status: 'completed',
+    },
+    {
+      id: 2,
+      title: 'Practice scss',
+      status: 'new',
+    },
+    {
+      id: 3,
+      title: 'Edit resume',
+      status: 'new',
+    },
+    {
+      id: 4,
+      title: 'Practice interview',
+      status: 'new',
+    },
+    {
+      id: 5,
+      title: 'Apply for front end engineer jobs',
+      status: 'new',
+    },
+  ];
 
-    const location = useLocation();
-    const history = useHistory();
-    const match = useRouteMatch();
-    const [todoList, setTodoList] = useState(initTodos);
+  const location = useLocation();
+  const history = useHistory();
+  const match = useRouteMatch();
+  const [todoList, setTodoList] = useState(initTodos);
 
-    const [filteredStatus, setFilteredStatus] = useState(() => {
-        const params = queryString.parse(location.search);
+  const [filteredStatus, setFilteredStatus] = useState(() => {
+    const params = queryString.parse(location.search);
 
-        return params.status || 'all';
+    return params.status || 'all';
+  });
+
+  // Listen to location search ---> update state
+  useEffect(() => {
+    const params = queryString.parse(location.search);
+
+    setFilteredStatus(params.status || 'all');
+  }, [location.search]);
+
+  function handleOnTodoClick(todo, idx) {
+    const newTodoList = [...todoList];
+
+    newTodoList[idx] = {
+      ...newTodoList[idx],
+      status: newTodoList[idx].status === 'new' ? 'completed' : 'new',
+    };
+
+    setTodoList(newTodoList);
+  }
+
+  function handleOnFormSubmit(formValues) {
+    // const newTodo = {
+    //     id: todoList.length + 1,
+    //     ...formValues,
+    // };
+    // setTodoList([...todoList, newTodo]);
+
+    console.log('Form submit: ', formValues);
+  }
+
+  function handleShowAllClick() {
+    // setFilteredStatus('all');
+    const queryParams = { status: 'all' };
+    history.push({
+      pathname: match.path,
+      search: queryString.stringify(queryParams),
     });
+  }
 
-    // Listen to location search ---> update state
-    useEffect(() => {
-        const params = queryString.parse(location.search);
+  function handleShowCompletedClick() {
+    // setFilteredStatus('completed');
+    const queryParams = { status: 'completed' };
+    history.push({
+      pathname: match.path,
+      search: queryString.stringify(queryParams),
+    });
+  }
 
-        setFilteredStatus(params.status || 'all');
-    }, [location.search]);
+  function handleShowNewClick() {
+    // setFilteredStatus('new');
+    const queryParams = { status: 'new' };
+    history.push({
+      pathname: match.path,
+      search: queryString.stringify(queryParams),
+    });
+  }
 
+  // render if and only if todoList or filteredStatus change
+  const renderedTodoList = useMemo(() => {
+    return todoList.filter((todo) => filteredStatus === 'all' || filteredStatus === todo.status);
+  }, [todoList, filteredStatus]);
 
-    function handleOnTodoClick(todo, idx) {
-        const newTodoList = [...todoList];
+  return (
+    <div className="todo-list-app">
+      <h1>React Hook: TodoList</h1>
 
-        newTodoList[idx] = {
-            ...newTodoList[idx],
-            status: newTodoList[idx].status === 'new' ? 'completed' : 'new',
-        }
+      <TodoForm onSubmit={handleOnFormSubmit} />
 
-        setTodoList(newTodoList);
-    };
+      <TodoFeature todos={renderedTodoList} onTodoClick={handleOnTodoClick} />
 
-    function handleOnFormSubmit(formValues) {
-        const newTodo = {
-            id: todoList.length + 1,
-            ...formValues,
-        };
-        setTodoList([...todoList, newTodo]);
-    };
-
-    function handleShowAllClick() {
-        // setFilteredStatus('all');
-        const queryParams = { status: 'all' };
-        history.push({
-            pathname: match.path,
-            search: queryString.stringify(queryParams),
-        });
-    };
-
-    function handleShowCompletedClick() {
-        // setFilteredStatus('completed');
-        const queryParams = { status: 'completed' };
-        history.push({
-            pathname: match.path,
-            search: queryString.stringify(queryParams),
-        });
-    };
-
-    function handleShowNewClick() {
-        // setFilteredStatus('new');
-        const queryParams = { status: 'new' };
-        history.push({
-            pathname: match.path,
-            search: queryString.stringify(queryParams),
-        });
-    };
-
-    // render if and only if todoList or filteredStatus change
-    const renderedTodoList = useMemo(() => {
-        return todoList.filter(todo => filteredStatus === 'all' || filteredStatus === todo.status);
-    }, [todoList, filteredStatus]);
-
-    return (
-        <div className="todo-list-app">
-            <h1>React Hook: TodoList</h1>
-
-            <TodoForm onSubmit={handleOnFormSubmit} />
-
-            <TodoFeature
-                todos={renderedTodoList}
-                onTodoClick={handleOnTodoClick}
-            />
-
-            <div>
-                <button onClick={handleShowAllClick}>Show All</button>
-                <button onClick={handleShowCompletedClick}>Show Completed</button>
-                <button onClick={handleShowNewClick}>Show New</button>
-            </div>
-        </div>
-    )
+      <div>
+        <button onClick={handleShowAllClick}>Show All</button>
+        <button onClick={handleShowCompletedClick}>Show Completed</button>
+        <button onClick={handleShowNewClick}>Show New</button>
+      </div>
+    </div>
+  );
 }
 
 export default ListPage;
